5637591f1c4636916462eb5f223a1c912cb97cc8
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2025-11-14 10:13:02 -0300

    Implement Animal interface with multiple classes

--- INTERFACES/Animal.java
+++ INTERFACES/Animal.java
@@ -1,1 +1,61 @@
+public interface Animal {
+    void emitirSom();
+    void mover();
+}
 
+
+public class Cachorro implements Animal {
+    
+    @Override
+    public void emitirSom(){
+        System.out.println("Au Au");
+    }
+    
+    @Override
+    public void mover(){
+        System.out.println("Cachorro deu 5 passos");
+    }
+}
+
+
+public class Gato implements Animal{
+    
+    @Override
+    public void emitirSom(){
+        System.out.println("Miau Miau");
+    }
+    
+    @Override
+    public void mover(){
+        System.out.println("Gato deu 5 passos");
+    }
+}
+
+public class Passaro implements Animal {
+    
+    @Override
+    public void emitirSom(){
+        System.out.println("Piu Piu");
+    }
+    @Override
+    public void mover(){
+        System.out.println("Passaro voou");
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+        Animal[] a = new Animal[3];
+        a[0] = new Cachorro();
+        a[1] = new Gato();
+        a[2] = new Passaro();
+        
+        for (Animal animal : a) {
+            animal.emitirSom();
+            animal.mover();
+            System.out.println("\n");
+        }
+    }
+}
